fba1c240b08c04bbef0173e5d95cee9178a82b06
NAJEEBKHAN123 <najeebkhanlaku@gmai.com>
2024-02-11 21:27:42 +0500

    addd

---
+++ first.js
@@ -0,0 +1,50 @@
+
+       // Q1 ---->
+
+// var num1 = +prompt("Enter the first number:");
+// var num2 = +prompt("Enter the second number:");
+
+// console.log("the sum of the all digits is ="+ (num1 + num2));
+
+//     //    Q2------>
+
+// let sum = prompt("Enter the number");
+
+
+
+
+
+
+    //   Q3------>
+
+    // let num = [34, 45, 76, 34, 65];
+    // sum = 0;
+    // for(let i=0; i<num.length; i++){
+    //     sum += num[i];
+
+    // }
+    // var average = num.length > 0 ? sum / num.length : 0;
+    // alert("The average is: " + average);
+
+
+
+
+// Q4----->
+
+
+// var string1 = prompt("Enter the first string:");
+// var string2 = prompt("Enter the second string:");
+
+// if (string1.length === string2.length) {
+//   alert("Both strings have same number of characters.");
+// } else if (string1.length > string2.length) {
+//   alert("The 1st string has more characters than the 2nd.");
+// } else {
+//   alert("The 2nd string has more characters than 1st .");
+// }
+
+
+    // Q5======>
+
+    let num1 = prompt("enter the first");
+    let num2 = prompt("entet the ")
